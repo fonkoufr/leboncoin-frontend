874011c3e5675dad9d453059efbe4ea9a9ac6f58
FONKOU DJITSOP <francis.fonkoudjitsop@cgi.com>
2026-02-12 15:29:19 +0100

    Mise à jour de l'URL vers le backend Render

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080/api";
+const API_URL = "https://leboncoin-backend-cvhr.onrender.com/api";
 
 // ══════════════════════════════════════════════
 // GESTION DES ANNONCES
